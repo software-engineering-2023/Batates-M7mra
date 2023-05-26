2e0ce33b798b7b9cd86aab4c32c2220572e6cee5
mostafa-elamory <102230910+mostafa-elamory@users.noreply.github.com>
2023-05-26 22:16:16 +0300

    new edit for the page

--- online-banking-system-website/src/app/report/page.tsx
+++ online-banking-system-website/src/app/report/page.tsx
@@ -1,7 +1,38 @@
 export default function Report() {
-    (
-  <p>mathiow</p>
-  
+    
+    return(
+        <form>
+        <div className="mb-3">
+          <label htmlFor="reportTitle" className="form-label">Report Title</label>
+          <input type="text" className="form-control" id="reportTitle" placeholder="Enter report title" />
+        </div>
+      
+        <div className="mb-3">
+          <label htmlFor="reportDescription" className="form-label">Report Description</label>
+          <textarea className="form-control" id="reportDescription" rows={5} placeholder="Enter report description"></textarea>
+        </div>
+      
+        <div className="mb-3">
+          <label htmlFor="reportCategory" className="form-label">Report Category</label>
+          <select className="form-control" id="reportCategory">
+            <option>Select a category</option>
+            <option>Technical Issues</option>
+            <option>Account Inquiries</option>
+            <option>Fraudulent Activity</option>
+            <option>Other</option>
+          </select>
+        </div>
+      
+        <div className="mb-3">
+          <label htmlFor="reportAttachment" className="form-label">Attach File </label>
+          <input type="file" className="form-control-file" id="reportAttachment" />
+        </div>
+      
+        <div className="d-flex justify-content-center">
+        <a href="http://localhost:3000/report/status" className="btn btn-primary">Submit Report</a>
+        </div>
+      </form>
+      
 
     )
 }
--- online-banking-system-website/src/app/report/status/page.tsx
+++ online-banking-system-website/src/app/report/status/page.tsx
@@ -1,5 +1,56 @@
+
 export default function Report() {
+   
     return (
-        <p>mathew hany</p>
-    )
+        <div className="container">
+          <h1 className="text-center">Report Status</h1>
+          
+            <div className="table-responsive">
+              <table className="table table-success table-dark table-hover">
+                <thead className="table-dark">
+                  <tr>
+                    <th scope="col">Report ID</th>
+                    <th scope="col">Report title</th>
+                    <th scope="col">Report Category</th>
+                    <th scope="col">Report Date</th>
+                    <th scope="col">Report Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+    <tr>
+      <td>52-7516</td>
+      <td>balabizo</td>
+      <td>Technical Issues</td>
+      <td>'2023-05-25'</td>
+      <td>Pending</td>
+    </tr>
+    <tr>
+      <td>52-7516</td>
+      <td>balabizo</td>
+      <td>Fraudulent Activity</td>
+      <td>'2023-05-26'</td>
+      <td>Resolved</td>
+    </tr>
+    <tr>
+      <td>52-7516</td>
+      <td>balabizo</td>
+      <td>Technical Issues</td>
+      <td>'2023-05-25'</td>
+      <td>In Progress</td>
+    </tr>
+  </tbody>
+              </table>
+            </div>
+           
+          <div className="position-absolute top-100 start-50 translate-middle">
+          <a href="http://localhost:3000/report" className="btn btn-primary btn-lg">Go to Report Page</a>
+          </div>
+          </div>
+          
+       
+      );
+
+
 }
+       
+    
