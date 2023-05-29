811420a129676708f86cc8c163aab4884b2a0cf8
Rafeeq Bassem <ultimaterafy@gmail.com>
2023-05-29 15:33:51 +0300

    fixed spelling errors and loan app drop down menus

--- online-banking-system-website/src/app/layout.tsx
+++ online-banking-system-website/src/app/layout.tsx
@@ -65,38 +65,23 @@ const sidebarLinks: SidebarLink[] = [
     userTypes: [UserType.CLIENT],
   },
   {
-<<<<<<< HEAD
     href: "/reminders",
     icon: "cash",
     text: "Reminders",
-=======
-    href: "/contact-us",
-    icon: "telephone",
-    text: "Contact Us",
     userTypes: [UserType.CLIENT],
-  },
-  {
-    href: "/remainders",
-    icon: "cash",
-    text: "Remainders",
-    userTypes: [UserType.CLIENT],
->>>>>>> 7964842282d9fe3397f500786e0d9568e9bb41d8
   },
 
   {
     href: "/transfers",
     icon: "arrow-left-right",
-<<<<<<< HEAD
-    text: "Bank Transfers"
+    text: "Bank Transfers",
+    userTypes: [UserType.CLIENT],
   },
   {
     href: "/contact-us",
     icon: "telephone",
     text: "Contact Us",
-=======
-    text: "Bank Transfers",
     userTypes: [UserType.CLIENT],
->>>>>>> 7964842282d9fe3397f500786e0d9568e9bb41d8
   },
 ];
 
@@ -146,86 +131,6 @@ export default function RootLayout({
         <title>Online Banking System</title>
       </head>
       <body data-bs-theme="dark">
-<<<<<<< HEAD
-        {/* <AlertsProvider> */}
-        <nav className="navbar navbar-expand-lg bg-body-tertiary d-block d-md-none fixed-top">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-              Bankaak
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {sidebarLinks.map((link, i) => (
-                  <li className="nav-item" key={i}>
-                    <a
-                      className={clsx(
-                        "nav-link",
-                        pathname == link.href && "active"
-                      )}
-                      aria-current="page"
-                      href={link.href}
-                    >
-                      <i className={"bi me-2 bi-" + link.icon}></i> {link.text}
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <div className="container-fluid h-100">
-          <div className="d-block d-md-none p-4"></div>
-          <div className="row h-100">
-            <div
-              className="col-md-4 col-xl-2 col-lg-3 d-md-flex flex-column flex-shrink-0 p-3 h-100 border-end fixed-top d-none bg-body-tertiary"
-              id="sidebar"
-            >
-              <Link
-                href="/"
-                className="d-flex align-items-center text-decoration-none link-body-emphasis"
-              >
-                <span className="fs-1">SKAB</span>
-              </Link>
-              <ul className="nav nav-pills flex-column my-auto">
-                {sidebarLinks.map((link, i) => (
-                  <li className="nav-item" key={i}>
-                    <Link
-                      className={clsx(
-                        "nav-link py-4 link-body-emphasis",
-                        pathname == link.href && "active"
-                      )}
-                      href={link.href}
-                    >
-                      <i className={`bi bi-${link.icon} me-3`}></i>
-                      {link.text}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <div className="d-flex justify-content-start align-items-center gap-3 border-top pt-4">
-                <img
-                  src="/ahmed-mohsen.png"
-                  height={45}
-                  width={45}
-                  className="rounded-circle"
-                />
-                Ahmed Mohsen
-=======
         <GlobalStateContext.Provider
           value={{ setShowSidebar, setUser, setLoading }}
         >
@@ -268,7 +173,6 @@ export default function RootLayout({
                     ))}
                   </ul>
                 </div>
->>>>>>> 7964842282d9fe3397f500786e0d9568e9bb41d8
               </div>
             </nav>
           )}
